46831310e2c63138b6b33e1d313bb6f6a1131fbd
Mrunal Bajaj <39082429+mrunalb@users.noreply.github.com>
2018-05-08 16:10:22 +0530

    Update Calculator.py

--- Basic/Calculator.py
+++ Basic/Calculator.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python  #Shebang line for python 
 
-num1 = input("Enter first number")
-num2 = input("Enter second number")
-add = num1 + num2
-sub = num1 - num2
-mul = num1 * num2 
-div = num1 / num2 
-print "Addition is:" , add
-print "Subtraction is:" , sub
-print "Multiplication is:" , mul
-print "Division is:" , div
+num1 = input("Enter first number")  #For accepting user entered value 
+num2 = input("Enter second number")  #For accepting user entered value
+add = num1 + num2  #Basic operations
+sub = num1 - num2  #Basic operations
+mul = num1 * num2  #Basic operations
+div = num1 / num2  #Basic operations
+print "Addition is:" , add  #For printing results
+print "Subtraction is:" , sub  #For printing results
+print "Multiplication is:" , mul  #For printing results
+print "Division is:" , div  #For printing results
