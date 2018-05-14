977b35f86ea863fd36edbdc41c0aa7496b1cf48c
Mrunal Bajaj <39082429+mrunalb@users.noreply.github.com>
2018-05-14 19:12:33 +0530

    replace function

---
+++ Basic/replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+print("We are replacing 'not bad' with 'good' in the given statement..!")
+str1 = input("Enter a statement which contains 'not bad' in it:")
+result = str1.replace("not bad","good")
+print(result)
